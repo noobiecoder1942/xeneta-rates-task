785c0017469534bfa3321d666a29c3cea481824e
noobiecoder1942 <abhiragnagpure@gmail.com>
2024-07-05 23:59:14 -0400

    Added more rates API tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,3 +11,7 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+@pytest.fixture
+def wellformed_rates_query(date_from="2016-01-01", date_to="2016-01-04", origin="CNSGH", destination="north_europe_main"):
+    return f"/rates?date_from={date_from}&date_to={date_to}&origin={origin}&destination={destination}"
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
 import pytest
 import json
+from werkzeug.exceptions import BadRequest
 
 def test_index_endpoint(client):
     response = client.get("/")
@@ -11,8 +12,8 @@ def test_healthcheck_endpoint(client):
     assert response.status_code == 200
     assert response.data.decode("utf-8") == "Service is up and running!"
 
-def test_rates_endpoint(client):
-    response = client.get("/rates?date_from=2016-01-01&date_to=2016-01-04&origin=CNSGH&destination=north_europe_main")
+def test_rates_endpoint_happy_path(client, wellformed_rates_query):
+    response = client.get(wellformed_rates_query)
     assert response.status_code == 200
     assert json.loads(response.data.decode("utf-8")) == [
         {"day": "2016-01-01", "average_price": 1112},
@@ -20,3 +21,32 @@ def test_rates_endpoint(client):
         {"day": "2016-01-03", "average_price": None},
         {"day": "2016-01-04", "average_price": None},
     ]
+
+def test_rates_endpoint_sad_path(client):
+    malformed_rates_query = "/rates?date_to=2016-13-10&origin=china_main&destination=scandinavia"
+    response = client.get(malformed_rates_query)
+    assert response.status_code == 400
+    response_data = response.data.decode("utf-8")
+    assert "Bad Request" in response_data
+    assert "Invalid date_from argument" in response_data
+
+    malformed_rates_query = "/rates?date_from=2016-01-10&origin=china_main&destination=scandinavia"
+    response = client.get(malformed_rates_query)
+    assert response.status_code == 400
+    response_data = response.data.decode("utf-8")
+    assert "Bad Request" in response_data
+    assert "Invalid date_to argument" in response_data
+
+    malformed_rates_query = "/rates?date_from=2016-01-01&date_to=2016-01-10&destination=scandinavia"
+    response = client.get(malformed_rates_query)
+    assert response.status_code == 400
+    response_data = response.data.decode("utf-8")
+    assert "Bad Request" in response_data
+    assert "Invalid origin argument" in response_data
+
+    malformed_rates_query = "/rates?date_from=2016-01-01&date_to=2016-01-10&origin=china_main"
+    response = client.get(malformed_rates_query)
+    assert response.status_code == 400
+    response_data = response.data.decode("utf-8")
+    assert "Bad Request" in response_data
+    assert "Invalid destination argument" in response_data
